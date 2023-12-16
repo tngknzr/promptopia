bebd42870e08d457a8608e759dec8958cbec8847
tngknzr <agustinparedes95@gmail.com>
2023-12-16 02:21:27 +0100

    tutorial

---
+++ components/Nav.jsx
@@ -0,0 +1,69 @@
+"use client"
+import React from 'react'
+import Link from 'next/link'
+import Image from 'next/image'
+import { useState, useEffect } from 'react'
+import {signIn, signOut, useSession, getProviders } from 'next-auth/react'
+const Nav = () => {
+
+    const isUserLoggedIn= true;
+    const [providers, setProviders] = useState(null);
+    useEffect(()=>{
+        const setProviders = async () => {
+            const response = await getProviders();
+        setProviders(response)
+        }
+        setProviders()
+    },[])
+  return (
+    <nav className='flex-between w-full mb-16 pt-3'>
+        <Link href="/" className='flex gap-2 flex-center'>
+            <Image
+            src="/assets/assets/images/logo.svg"
+            alt="Promptopia Logo"
+            width={30}
+            height={30}
+            className='object-contain'/>
+            <p className='logo_text'>Promptopia</p>
+        </Link>
+        <div className='sm:flex hiddem'>
+            {isUserLoggedIn ? (
+                <div className='flex gap-3 md:gap-5'>
+                    <Link href="/create-prompt" className='black_btn'>
+                        Create Post
+                    </Link>
+                    <button type='button' className='outline_btn' onClick={signOut}>
+                        Sign Out
+                    </button>
+                    <Link href="/profile">
+                        <Image
+                        src="/assets/assets/images/logo.svg"
+                        width={37}
+                        height={37}
+                        className='rounded-full'
+                        alt='profile'
+                        />
+                    </Link>
+                </div>
+            ):(
+                <>
+                  {providers && 
+                  Object.values(providers).map((provider)=>(
+                    <button
+                    type='button'
+                    key={provider.name}
+                    onClick={()=> signIn(provider.id)}
+                    className='black_btn'
+                    >
+                        Sign In
+                    </button>
+                  ))}  
+                </>
+            )}
+
+        </div>
+    </nav>
+  )
+}
+
+export default Nav
