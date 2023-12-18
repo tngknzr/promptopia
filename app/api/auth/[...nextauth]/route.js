f0615cd9d4f2792850009b428f917fb1ff4ed676
tngknzr <agustinparedes95@gmail.com>
2023-12-18 20:28:58 +0100

    tutorial

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -2,7 +2,7 @@ import nextAuth from "next-auth";
 import NextAuth from "next-auth";
 import GoogleProvider from 'next-auth/providers/google'
 import { connectToDB } from "@utils/database";
-
+import User from "@models/user";
 const handler = nextAuth({
     providers: [
         GoogleProvider({
@@ -11,22 +11,39 @@ const handler = nextAuth({
 
         })
     ],
-    async session ({session}) {
-        try {
-
-        } catch (error) {
-            
-        }
-    },
-    async signIn({profile}) {
-        try {
-            await connectToDB();
-            return true;
-        } catch (error) {
-            console.log(error)
-            return false
+    callbacks: {
+        async session ({session}) {
+        
+            const sessionUser = await User.findOne({
+                email: session.user.email
+            })
+            session.user.id= sessionUser._id.toString()
+            return session;
+        },
+        async signIn({profile}) {
+            try {
+                await connectToDB();
+    
+                const userExists = await User.findOne({
+                    email: profile.email
+                });
+    
+                if (!userExists) {
+                    await User.create({
+                        email: profile.email,
+                        username: profile.name.replace(' ', '').toLowerCase(),
+                        image: profile.picture
+                    })
+                }
+    
+                return true;
+            } catch (error) {
+                console.log(error)
+                return false
+            }
         }
     }
+   
 })
 
 export {handler as GET, handler as POST}
--- components/Nav.jsx
+++ components/Nav.jsx
@@ -8,16 +8,16 @@ import {signIn, signOut, useSession, getProviders } from 'next-auth/react'
 
 const Nav = () => {
 
-    const isUserLoggedIn= true;
+    const { data: session} = useSession()
     const [providers, setProviders] = useState(null);
     const [toggleDropdown,setToggleDropdown] = useState(false)
     
     useEffect(()=>{
-        const setProviders = async () => {
+        const setUpProviders = async () => {
             const response = await getProviders();
         setProviders(response)
         }
-        setProviders()
+        setUpProviders()
     },[])
   
     return (
@@ -31,8 +31,9 @@ const Nav = () => {
             className='object-contain'/>
             <p className='logo_text'>Promptopia</p>
         </Link>
+        {/* {alert (session?.user)} */}
         <div className='sm:flex hidden'>
-            {isUserLoggedIn ? (
+            {session?.user ? (
                 <div className='flex gap-3 md:gap-5'>
                     <Link href="/create-prompt" className='black_btn'>
                         Create Post
@@ -42,7 +43,7 @@ const Nav = () => {
                     </button>
                     <Link href="/profile">
                         <Image
-                        src="/assets/assets/images/logo.svg"
+                        src={session?.user.image}
                         width={37}
                         height={37}
                         className='rounded-full'
@@ -69,10 +70,10 @@ const Nav = () => {
         </div>
 
         <div className='sm:hidden flex relative'>
-            {isUserLoggedIn ? (
+            {session?.user ? (
                 <div className='flex'>
                     <Image
-                        src="/assets/assets/images/logo.svg"
+                        src={session?.user.image}
                         width={37}
                         height={37}
                         className='rounded-full'
